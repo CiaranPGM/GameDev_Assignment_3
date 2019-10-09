2a5506705e572be178940d3b7b754b231c5cce2e
Ciaran Mulholland <ciaranpgm@hotmail.com>
2019-10-10 01:38:56 +1100

    Added alot more functionality with movement and other things

---
+++ 12936833_A3_project/Assets/Scripts/Player_Eat.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Player_Eat : MonoBehaviour
+{
+    private int playerScore = 0;
+    public new AudioSource[] audio;
+    public Text scoreText;
+
+    void Start()
+    {
+        SetScoreText();
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if(other.gameObject.tag == "PacDot")
+        {
+            Destroy(other.gameObject);
+            playerScore += 10;
+            audio[0].Play();
+            scoreText.text = "Score: " + playerScore.ToString();
+        }
+        else if(other.gameObject.tag == "PowerUpDot")
+        {
+            Destroy(other.gameObject);
+            playerScore += 50;
+            audio[1].Play();
+            scoreText.text = "Score: " + playerScore.ToString();
+        }
+    }
+
+    void SetScoreText()
+    {
+        scoreText.text = "Score: " + playerScore.ToString();
+    }
+
+    public int GetPlayerScore()
+    {
+        return playerScore;
+    }
+
+}
--- 12936833_A3_project/Assets/Scripts/Player_Movement.cs
+++ 12936833_A3_project/Assets/Scripts/Player_Movement.cs
@@ -5,72 +5,97 @@ using UnityEngine;
 public class Player_Movement : MonoBehaviour
 {
     private Vector2 movement;
-    public Vector3 lookVector;
-    private float moveSpeed = 0.1f;
+    public Vector2 direction = Vector2.zero;
+    private float moveSpeed = 10f;
     Animator animator;
-    // Start is called before the first frame update
+    public Transform portalDestinationBR, portalDestinationBL, portalDestinationTR, portalDestinationTL;
+
     void Start()
     {
         animator = gameObject.GetComponent<Animator>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        GetMovementInput();
         ManageInput();
-        CharacterRotation();
-        CharacterPosition();
-    }
-
-    void GetMovementInput()
-    {
-        movement.x = Input.GetAxis("Horizontal");
-        movement.y = Input.GetAxis("Vertical");
-        Debug.Log(movement);
+        Move();
+        Rotate();
     }
 
     void ManageInput()
     {
-        //'d' movement
+        movement.x = Input.GetAxis("Horizontal");
+        movement.y = Input.GetAxis("Vertical");
+        if(movement != Vector2.zero)
+        {
+            animator.SetBool("isMoving", true);
+        }
+        else { animator.SetBool("isMoving", false); }
+    }
+
+    void Move()
+    {
         if (movement.x == 1.0f)
         {
-            lookVector.z = 90f;
-            animator.SetBool("MoveRight", true);
-        }
-        else if(movement.x < 1.0f){
-            lookVector.z = 0f;
-            animator.SetBool("MoveRight", false);
-        }
-
-        //'a' movement
-        if(movement.x == -1.0f)
+            movement.y = 0.0f;
+            transform.localPosition += (Vector3)(movement * moveSpeed) * Time.deltaTime;
+        }else if(movement.y == 1.0f)
         {
-            lookVector.z = -90f;
-        }
-
-        //'w' movement
-        if(movement.y == 1.0f)
+            movement.x = 0.0f;
+            transform.localPosition += (Vector3)(movement * moveSpeed) * Time.deltaTime;
+        }else if(movement.x == -1.0f)
         {
-            lookVector.z = -180f;
+            movement.y = 0.0f;
+            transform.localPosition += (Vector3)(movement * moveSpeed) * Time.deltaTime;
+        }else if(movement.y == -1.0f)
+        {
+            movement.x = 0.0f;
+            transform.localPosition += (Vector3)(movement * moveSpeed) * Time.deltaTime;
         }
     }
 
-    void CharacterRotation()
+    void Rotate()
     {
-        Vector2 zero = new Vector2(0,0);
-        if(movement != zero)
+        if(movement == Vector2.left)
         {
-            transform.rotation = Quaternion.LookRotation(lookVector);
+            transform.localScale = new Vector3(-1, 1, 1);
+            transform.localRotation = Quaternion.Euler(0, 0, 0);
 
-            //gameObject.transform.rotation = Quaternion.Euler(transform.eulerAngles + new Vector3(0,0,90f));
-
-            //gameObject.transform.parent.eulerAngles = new Vector3(0, 0, 90);
+        }
+        else if(movement == Vector2.right)
+        {
+            transform.localScale = new Vector3(1, 1, 1);
+            transform.localRotation = Quaternion.Euler(0, 0, 0);
+        }
+        else if (movement == Vector2.up)
+        {
+            transform.localScale = new Vector3(1, 1, 1);
+            transform.localRotation = Quaternion.Euler(0, 0, 90);
+        }
+        else if (movement == Vector2.down)
+        {
+            transform.localScale = new Vector3(1, 1, 1);
+            transform.localRotation = Quaternion.Euler(0, 0, 270);
         }
     }
 
-    void CharacterPosition()
+    void OnCollisionEnter2D(Collision2D other)
     {
-        gameObject.transform.Translate(movement * moveSpeed, Space.World);
+        if(other.collider.gameObject.name == "Portal_Bottom_Right")
+        {
+            transform.position = portalDestinationBL.position + new Vector3(1.5f,0);
+        }else if (other.collider.gameObject.name == "Portal_Bottom_Left")
+        {
+            transform.position = portalDestinationBR.position - new Vector3(1.5f, 0);
+        }
+        else if (other.collider.gameObject.name == "Portal_Top_Right")
+        {
+            transform.position = portalDestinationTL.position + new Vector3(1.5f, 0);
+        }
+        else if (other.collider.gameObject.name == "Portal_Top_Left")
+        {
+            transform.position = portalDestinationTR.position - new Vector3(1.5f, 0);
+        }
+
     }
 }
