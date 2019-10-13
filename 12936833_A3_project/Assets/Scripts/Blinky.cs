69704019197e26052aa2ab87bb2994d30185aaea
Ciaran Mulholland <ciaranpgm@hotmail.com>
2019-10-13 20:45:08 +1100

    Made an attempt on AI

---
+++ 12936833_A3_project/Assets/Scripts/Blinky.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Blinky : MonoBehaviour
+{
+    private float moveSpeed = 3.5f;
+
+    private GameObject msPacMan;
+
+    public Transform[] waypoints;
+    int current = 0;
+    Animator anim;
+
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        anim.SetFloat("DirX", 0);
+        anim.SetFloat("DirY", 0);
+    }
+
+    //void FixedUpdate()
+    //{
+    //    if(transform.position != waypoints[current].position)
+    //    {
+    //        Vector2 p = Vector2.MoveTowards(transform.position, waypoints[current].position, moveSpeed);
+    //        GetComponent<Rigidbody2D>().MovePosition(p);
+    //    }
+    //    else { current = (current + 1) % waypoints.Length; }
+    //}
+
+    void Update()
+    {
+        
+    }
+}
